6927771a6748e497870e9dfa0e13e7ab305c0646
LonlyDWolf <contact@ahmadmollaei.dev>
2026-04-29 21:21:08 +0300

    zsh: remove unused functions

--- zsh/.config/zsh/07-functions.zsh
+++ zsh/.config/zsh/07-functions.zsh
@@ -72,43 +72,6 @@ function y() {
   fi
 }
 
-# Enhanced CD with FZF - Interactive directory selection
-function cdi() {
-  if ! command_exists fd || ! command_exists fzf; then
-    error_log "cdi requires fd and fzf"
-    return 1
-  fi
-  
-  local dir
-  dir=$(fd --type d --follow --exclude .git --exclude node_modules --exclude .venv \
-    --exclude Library --exclude Applications --exclude Desktop --exclude Downloads \
-    --exclude Movies --exclude Music --exclude Pictures --exclude Public --exclude .Trash \
-    . "$HOME" | \
-    fzf --prompt='  ' \
-        --header='Select directory (from ~)' \
-        --border-label=' Change Directory ' \
-        --preview 'eza --tree --level=2 --icons --color=always {}' \
-        --bind 'ctrl-/:toggle-preview')
-  [[ -n "$dir" ]] && cd "$dir"
-}
-
-# Quick edit with FZF - Find and edit files
-function vf() {
-  if ! command_exists fd || ! command_exists fzf; then
-    error_log "vf requires fd and fzf"
-    return 1
-  fi
-  
-  local file
-  file=$(fd --type f --hidden --exclude .git --exclude node_modules | \
-    fzf --prompt='  ' \
-        --header='Select file to edit' \
-        --border-label=' Edit File ' \
-        --preview 'bat -n --color=always --line-range :500 {}' \
-        --bind 'ctrl-/:toggle-preview')
-  [[ -n "$file" ]] && nvim "$file"
-}
-
 # Process killer with FZF
 function fkill() {
   if ! command_exists fzf; then
@@ -121,44 +84,6 @@ function fkill() {
   if [[ -n "$pid" ]]; then
     echo "$pid" | xargs kill -"${1:-9}"
   fi
-}
-
-# JJ: Interactive change switcher
-function jjsw() {
-  if ! command_exists jj; then
-    error_log "jjsw requires jj (Jujutsu)"
-    return 1
-  fi
-  
-  local change
-  change=$(jj log --no-graph --color=always -r 'all()' -T 'change_id.short() ++ " " ++ description.first_line()' | \
-    fzf --ansi \
-        --prompt='  ' \
-        --header='Select change to edit' \
-        --border-label=' JJ Changes ' \
-        --preview 'jj show --color=always -r {1}' \
-        --preview-window=right:60% \
-        --bind 'ctrl-/:toggle-preview' | \
-    awk '{print $1}')
-  [[ -n "$change" ]] && jj edit "$change"
-}
-
-# JJ: Interactive file selection for diff
-function jjf() {
-  if ! command_exists jj; then
-    error_log "jjf requires jj (Jujutsu)"
-    return 1
-  fi
-  
-  local file
-  file=$(jj file list | \
-    fzf --prompt='  ' \
-        --header='Select file to view diff' \
-        --border-label=' JJ Files ' \
-        --preview 'jj diff {}' \
-        --preview-window=right:60% \
-        --bind 'ctrl-/:toggle-preview')
-  [[ -n "$file" ]] && echo "$file"
 }
 
 debug_log "Custom functions loaded"
